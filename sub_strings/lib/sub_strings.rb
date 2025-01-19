9d7fa71f94c63b39f195ed1cb919202d0a1be02b
kristin-13 <fabian.kristin.e@gmail.com>
2025-01-19 17:44:44 -0500

    add method for finding substrings and rspec

--- sub_strings/lib/sub_strings.rb
+++ sub_strings/lib/sub_strings.rb
@@ -1,2 +1,12 @@
 class SubStrings
+  def find_substrings(string,substring_array)
+    hash = Hash.new
+    substring_array.each do |substring|
+      occurrences = string.scan(substring)
+      if occurrences.length > 0
+        hash[substring] = occurrences.length
+      end
+    end
+    return hash
+  end
 end
